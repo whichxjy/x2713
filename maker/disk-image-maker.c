c22b64f39a13c7ab6e672984af160a8c3aed6047
whichxjy <whichxjy@gmail.com>
2019-04-17 01:54:36 +0800

    Add maker

---
+++ maker/disk-image-maker.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        exit(1);
+    }
+
+    char buffer[512];
+
+    FILE* fp = fopen(argv[1], "rb");
+    int size = fread(buffer, 1, 512, fp);
+    if (size > 510) {
+        exit(1);
+    }
+    fclose(fp);
+
+    memset(buffer + size, 0, 510 - size);
+    buffer[510] = 0x55;
+    buffer[511] = 0xAA;
+    fwrite(buffer, 1, 512, stdout);
+
+    memset(buffer, 0, 512);
+    for (int i = 0; i < 1023; i++) {
+        fwrite(buffer, 1, 512, stdout);
+    }
+    
+    return 0;
+}
